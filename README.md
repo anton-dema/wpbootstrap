d150ee0c80b5f5b3f9026133d6e4ecb60305adf0
anton-dema <anton.demartini@gmail.com>
2012-10-20 09:30:45 +0200

    Stripped Anton personalized footer.php

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ This wordpress template has been built from this howto http://blog.teamtreehouse
 and css has been taken from http://bootswatch.com/cerulean/
 
 Not a big deal, but feel free to fork it, if you need it. 
+
+Punct
--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
       <hr>
 
       <footer>
-        <p><img src=http://dema.tv/wp-content/uploads/2012/10/somerights20.png> <h4>ogni contenuto è coperto da licenza CC BY-NC-SA 3.0</h4></p>
-        <h6>Sono molto "old school" e ne sono fiero :-)</h6>
+        <p><img src=""> <h4>Company</h4></p>
       </footer>
 
     </div> <!-- /container -->
